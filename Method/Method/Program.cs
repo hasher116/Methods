2715aaf7432d362cbc440a4f9779a547c26f3fc1
hasher116 <andrei13081996@mail.ru>
2021-03-06 06:29:48 +0300

    Initial commit

---
+++ Method/Method/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(sum(y:2, x:3)) ;
+        }
+
+        static int sum(int x, int y, int z = 5, int u = 3)
+        {
+            return x + y + z + u;
+        }
+    }
+}
